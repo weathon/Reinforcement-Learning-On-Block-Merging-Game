afd51da077fafe8a966bc2a245704982eb42c0a7
weathon <wguo6358@gmail.com>
2022-03-25 15:42:29 +0000

    finished push and fixed cor

--- game.py
+++ game.py
@@ -1,7 +1,7 @@
 import numpy as np
 import random 
 
-field = np.zeros((5,6))
+field = np.zeros((6,6))
 steps = 0
 score = 0
 
@@ -12,8 +12,21 @@ def display():
             if field[i,j]== 0:
                 print("_",end="\t")
             else:
-                print(int(field[i,j]))
+                print(int(field[i,j]),end="\t")
         print()
+
+
+
+def push_block(col, num):
+    if field[5][col] != 0:
+        print("Game over!")
+        quit()
+    for i in range(4,-1,-1):
+        if field[i][col] != 0:
+            field[i+1][col] = num
+            return
+    field[0][col] = num
+    
 
 def check_merging():
     pass
@@ -22,4 +35,6 @@ while 1:
     display()
     next_number = 2**random.choice([1,1,1,1,1,1,2,2,2,2,2,3,3])
     print("The next number is: %s" % next_number)
-    ans = input("Which col (starts with 0) you want to put it to? ")  
+    ans = int(input("Which col (starts with 0) you want to put it to? "))
+    push_block(ans,next_number)
+    check_merging()
