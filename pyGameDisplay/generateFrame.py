841f6b0a9e3621dba014910063ab97a9eeca9437
weathon <wguo6358@gmail.com>
2022-03-27 22:49:13 +0000

    Finished font size and color

--- pyGameDisplay/generateFrame.py
+++ pyGameDisplay/generateFrame.py
@@ -1,4 +1,4 @@
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFont
 import numpy as np
 
  
@@ -22,19 +22,22 @@ def generateFrame(frameNumber, field, nextNumber):
         color_2048 = (237, 194, 46)
         font_light = (249, 246, 242)
         font_dark = (119, 110, 101)
-
+        
+        font = ImageFont.truetype("/var/www/html/nextcloud/core/fonts/NotoSans-Bold.ttf", size=12)
 
         d = ImageDraw.Draw(img)
-        d.text((200, 585), 'Next Number is: ' + str(nextNumber))
+        d.text((200, 575), 'Next Number is: ' + str(nextNumber), font = font)
         for col in range (0,6):
                 for height in range (0,7):
+                        font = ImageFont.truetype("/var/www/html/nextcloud/core/fonts/NotoSans-Bold.ttf", size=12)
+
                         if height == 0: #header
                                 fontColor = font_dark
                                 row = height
                                 d.rounded_rectangle((83 * (col),  83 * (row), 83 * (col+1), 83 * (row+1)), fill=font_light, outline=font_dark, width=3, radius=7)
                                 #d.text((83 * (col), 83 * (row)), str(col) + ' ' + str(row), fill=(255,255,0)) #adds row / col for debugging
-                                d.text((83 * (col) + 83/4, 83 * (row)+  83/4), "column:", fill=fontColor)
-                                d.text((83 * (col) + 83/2, 83 * (row)+  83/2), str(col), fill=fontColor)
+                                d.text((83 * (col) + 83/4, 83 * (row)+  83/4), "column:",font = font, fill=fontColor)
+                                d.text((83 * (col) + 83/2, 83 * (row)+  83/2), str(col),font = font, fill=fontColor)
                         else:
                                 row = height - 1
                                 if field[row][col] == 0:
@@ -61,23 +64,37 @@ def generateFrame(frameNumber, field, nextNumber):
                                         d.rounded_rectangle((83 * (col),  83 * (height), 83 * (col+1), 83 * (height+1)), fill=color_512, outline=(187, 173, 160), width=3, radius=7)
                                 if field[row][col] == 1024:
                                         d.rounded_rectangle((83 * (col),  83 * (height), 83 * (col+1), 83 * (height+1)), fill=color_1024, outline=(187, 173, 160), width=3, radius=7)
-                                if field[row][col] == 2048:
+                                if field[row][col] >= 2048:
                                         d.rounded_rectangle((83 * (col),  83 * (height), 83 * (col+1), 83 * (height+1)), fill=color_2048, outline=(187, 173, 160), width=3, radius=7)
 
                                 # d.rounded_rectangle((83 * (col),  83 * (row), 83 * (col+1), 83 * (row+1)), fill=(0, 192, 192), outline="yellow",
                                 #    width=1, radius=7)
-                                if field[row][col] < 8:
-                                        fontColor = font_dark
-                                else:
-                                        fontColor = font_light
-                        
+                                fontColor = font_dark
+
+                                font_size = 40
+                                offsetCol = 30
+                                offsetRow = 0
+                                if field[row][col] > 10:
+                                        font_size = 30
+                                        offsetCol = 25
+                                        offsetRow = 10
+                                if field[row][col] > 100:
+                                        font_size = 25
+                                        offsetCol = 20
+                                        offsetRow = 20
+                                if field[row][col] > 1000:
+                                        font_size = 20
+                                        offsetRow = 25
+
+                                font = ImageFont.truetype("/var/www/html/nextcloud/core/fonts/NotoSans-Bold.ttf", size=font_size)
                                 #d.text((83 * (col), 83 * (height)), str(col) + ' ' + str(height), fill=(255,255,0)) #adds row / col for debugging
-                                d.text((83 * (col) + 83/2, 83 * (height)+  83/2), str(int(field[row][col])), fill=fontColor)
+                                d.text((83 * (col) + offsetCol, 83 * (height)+ offsetRow),str(int(field[row][col])), font = font, fill=fontColor)
 
         
-        img.save('pngs'frame_' + str(frameNumber) + '.png')
+        img.save('pngs/' + 'frame_' + str(frameNumber) + '.png')
 
-#test code
+
+# test code
 # field = np.zeros((6,6))
 # field[0][0] = 0
 # field[0][1] = 2
@@ -91,6 +108,9 @@ def generateFrame(frameNumber, field, nextNumber):
 # field[1][3] = 512
 # field[1][4] = 1024
 # field[1][5] = 2048
+# field[1][5] = 2048
+# field[2][0] = 4096
 
 # generateFrame(1,field,1)
+ #       img.save( 'frame_' + str(frameNumber) + '.png')
 
